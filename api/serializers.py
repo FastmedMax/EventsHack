4efbeb8efce96a204786714d87229b002a3418a4
fastmed <maks.memelige@gmail.com>
2022-02-22 21:42:14 +0200

    Add ReviewSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from .models import Review, Case, Callback, Event
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
