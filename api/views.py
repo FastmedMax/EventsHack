4fc5f657941d3b9fe156783c6d33754b7e149a88
fastmed <maks.memelige@gmail.com>
2022-02-22 21:46:52 +0200

    Update imports

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+
+from .models import Review, Callback, Case, Event
+from .serializers import ReviewSerializer, CaseSerializer, CaseFullSerializer, EventSerializer, CallBackSerializer
 
 # Create your views here.
