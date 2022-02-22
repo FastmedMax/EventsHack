21b7f057e9186ab9bd7856964245eddbe40f9654
fastmed <maks.memelige@gmail.com>
2022-02-22 21:47:11 +0200

    Add RewiewListView

--- api/views.py
+++ api/views.py
@@ -7,3 +7,18 @@ from .models import Review, Callback, Case, Event
 from .serializers import ReviewSerializer, CaseSerializer, CaseFullSerializer, EventSerializer, CallBackSerializer
 
 # Create your views here.
+class RewiewListView(ListAPIView):
+    queryset = Review
+    serializer_class = ReviewSerializer
+
+    def get(self, request, id=None, *args, **kwargs):
+        try:
+            if id:
+                reviews = self.queryset.objects.filter(case=id)
+            else:
+                reviews = self.queryset.objects.order_by("?")[:5]
+        except Review.DoesNotExist:
+            return Response("Отзывы не найдены!", status=status.HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
