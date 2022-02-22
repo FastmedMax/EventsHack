3c22cf4eb6ae971c47147ae1f2d9efd414386481
fastmed <maks.memelige@gmail.com>
2022-02-22 21:39:01 +0200

    Add admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import CasePhoto, Case, Callback, Event, Review
+
 # Register your models here.
+admin.site.register(CasePhoto)
+admin.site.register(Case)
+admin.site.register(Callback)
+admin.site.register(Event)
+admin.site.register(Review)
